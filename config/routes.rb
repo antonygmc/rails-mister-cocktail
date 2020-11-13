9d6a4730530f7041e180a04440a04f5b146042d0
Antony Marques <antonymarques97@gmail.com>
2020-11-13 15:28:06 +0100

    saving files bf pushing them on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: 'pages#home'
   root to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create, :delete]
